d3f7a06e377aa086356fd3bcfb3dbca48fbc089a
James Ayvaz <james.ayvaz@gmail.com>
2015-02-14 01:25:25 -0600

    added getValueOf method to parse strings back into enum

--- rokontrol-library/src/main/java/com/github/ayvazj/rokontrol/RokuKey.java
+++ rokontrol-library/src/main/java/com/github/ayvazj/rokontrol/RokuKey.java
@@ -15,8 +15,7 @@ public enum RokuKey {
     INFO("info"),
     BACKSPACE("backspace"),
     SEARCH("search"),
-    ENTER("enter")
-    ;
+    ENTER("enter");
 
     public final String endpoint;
 
@@ -27,5 +26,14 @@ public enum RokuKey {
     @Override
     public String toString() {
         return this.endpoint;
+    }
+
+    public static RokuKey getValueOf(String value) {
+        for (RokuKey v : values()) {
+            if (v.endpoint.equalsIgnoreCase(value)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException();
     }
 }
